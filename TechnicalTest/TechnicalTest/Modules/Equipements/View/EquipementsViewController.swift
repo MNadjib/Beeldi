9385c739853f75053e63ccb72d5bd1c4df8eb369
nbellouni <nbellouni@c2s.fr>
2019-12-19 00:03:59 +0100

    Project structure

---
+++ TechnicalTest/TechnicalTest/Modules/Equipements/View/EquipementsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EquipementsViewController.swift
+//  TechnicalTest
+//
+//  Created by nbellouni on 15/12/2019.
+//  Copyright © 2019 c2s. All rights reserved.
+//
+
+import UIKit
+
+class EquipementsViewController: UIViewController {
+     var viewModel: EquipementsViewModel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
